ee6cbd644045d99148484d6d65bb8a0193816598
[Ambattz} <[alvingeorge30@gmail.com]>
2022-06-20 00:55:06 +0530

    Complete API builds

--- NodeJS/src/routers/index.js
+++ NodeJS/src/routers/index.js
@@ -3,19 +3,60 @@ const Fixture = require("../models/fixture");
 
 const router = new express.Router();
 
-// Your code goes here
-// Write a route to get fetch the matches i.e., GET /fixtures
-// You should also implement below filters
-//   * filter to list matches that will be held between given start and end date
-//   * filter for venue
+router.get("/fixtures", (req, res) => {
+    var query = req.query;
+    var filter = {};
+    for (const params in query) {
+        if (params == "start_date") {
+            filter = {
+                ...filter,
+                date: { "$gte": new Date(query[params]) }
+            };
+        }
+        if (params == "end_date") {
+            filter = {
+                ...filter,
+                date: { ...filter.date, "$lte": new Date(query[params]) }
+            };
+        }
+        if (params === "venue") {
+            filter = {
+                ...filter,
+                venue: query[params]
+            }
+        }
+    }
+    Fixture
+        .find(filter, (err, matches) => {
+            if (err) {
+                res.status(400).send();
+            }
+            Fixture.countDocuments(filter, (err, count) => {
+                res.status(200).send({ count: count, records: matches });
+            })
+        }
+        );
 
 
-// Write a route to create a match fixture i.e., POST /fixtures
-// POST route will take all of these below params
-//   * team1
-//   * team2
-//   * venue
-//   * date
+});
+
+//Post for "/fixtures"
+router.post("/fixtures", (req, res) => {
+    const newMatch = new Fixture({
+        team1: req.body.team1,
+        team2: req.body.team2,
+        venue: req.body.venue,
+        date: req.body.date
+    })
+    newMatch
+        .save()
+        .then((result) => {
+            res.status(200).send(result)
+        })
+        .catch((err) => {
+            res.status(400).send("Error");
+        });
+});
 
 
 module.exports = router;
